1bdc4a187a95e366e807c78e2098c99a1c69e2cf
JGWill <jgi@jgwill.com>
2025-11-18 04:47:48 -0500

    local launch with session and tracing for claude online

---
+++ LAUNCH__session_id__avaFlowiseAgenticFlywheel_2511180446.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. _env.sh
+export SESSION_ID=5a90248b-cba1-4a79-9131-0c60ea23c441
+export session_id__avaFlowiseAgenticFlywheel_2511180446
+export session_id__avaFlowiseAgenticFlywheel_2511180446__MCP_CONFIG
+export session_id__avaFlowiseAgenticFlywheel_2511180446__ADD_DIR
+claude "we will want to develop @src/agentic_flywheel/ into this very own Flowise context, I guess that first, we will create a ./rispecs/ and use @__llms/llms-rise-framework.txt to create the specs that we need for that MCP, it was developped outside of here, therefore, it seemed relevant to explore extending its usage within this context for enhancing the work in here.  Given that it will be capable to store the chatflow being called by the Agentic Flywheel within Redis and that we would have potential tracing in langfuse, the coaiapy_aetherial MCP for example could be used to get the result of the inquiry being sent " --mcp-config $session_id__avaFlowiseAgenticFlywheel_2511180446__MCP_CONFIG --add-dir $session_id__avaFlowiseAgenticFlywheel_2511180446__ADD_DIR --session-id $session_id__avaFlowiseAgenticFlywheel_2511180446
---
+++ _env.sh
@@ -0,0 +1,9 @@
+
+session_id__avaAgenticFlywheelFlowiseLangflowCommonTracingSessionID_2511180358=a50f3fc2-eb8c-434d-a37e-ef9615d9c07d
+  
+# Namespace: avaFlowiseAgenticFlywheel 
+session_id__avaFlowiseAgenticFlywheel_2511180446=5a90248b-cba1-4a79-9131-0c60ea23c441
+alias cdavaFlowiseAgenticFlywheel="cd /src/_sessiondata/5a90248b-cba1-4a79-9131-0c60ea23c441 && ls -ltra" 
+session_id__avaFlowiseAgenticFlywheel_2511180446__MCP_CONFIG="/src/.mcp.coaiapy.env.aetherial.json /src/.mcp.github.ava.json"
+session_id__avaFlowiseAgenticFlywheel_2511180446__ADD_DIR="/src/_sessiondata/ /src/Miadi-46 /src/coaiapy /src/IAIP"
+  
